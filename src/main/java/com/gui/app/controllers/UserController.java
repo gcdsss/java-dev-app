79139e2395e4de62cf078729e03d36c02adaf619
gui <changdig@foxmail.com>
2025-09-09 16:42:43 +0800

    修改配置文件，添加调试接口

--- src/main/java/com/gui/app/controllers/UserController.java
+++ src/main/java/com/gui/app/controllers/UserController.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 
 import com.gui.app.entity.User;
 import com.gui.app.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 public class UserController {
@@ -24,7 +25,7 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/user")
-    public Map<String, Object> getUser(@RequestParam Map<String, String> params, @RequestBody String rawBodyString) {
+    public List<User> getUser(@RequestParam Map<String, String> params, @RequestBody String rawBodyString) {
         logger.info("getUser param: {}", params);
         logger.info("getUser body: {}", rawBodyString);
 
@@ -38,7 +39,15 @@ public class UserController {
         response.put("total", users.size());
 
         logger.info("查询到用户数量: {}", users.size());
-        return response;
+        return users;
+    }
+
+    @PostMapping("/user")
+    public User createUser(@RequestBody User user) {
+        // 创建用户
+        userService.save(user);
+
+        return userService.getUserById(user.getId());
     }
 
 }
